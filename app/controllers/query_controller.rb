453efdc9b1e3afd9cdae1a47a798f656303d6c5f
Chris Woodside <christopherwoodside@gmail.com>
2016-12-06 16:07:48 -0600

    Added Index controller and Index view to show daily forecasts.

--- app/controllers/query_controller.rb
+++ app/controllers/query_controller.rb
@@ -1,6 +1,13 @@
 class QueryController < ApplicationController
   def index
-    @queries = Unirest.get("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22chicago%2C%20il%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys").body
+    city = params[:city] || "Chicago"
+    state = params[:state] || "IL"
+    @data = Unirest.get("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22#{city}%2C%20#{state}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys").body
+    @temperature = @data["query"]["results"]["channel"]["item"]["condition"]["temp"]
+    @forecasts = @data["query"]["results"]["channel"]["item"]["forecast"]
+    @city = @data["query"]["results"]["channel"]["location"]["city"]
+    @country = @data["query"]["results"]["channel"]["location"]["country"]
+    @region = @data["query"]["results"]["channel"]["location"]["region"]
     render 'index.html.erb'
   end
 end
